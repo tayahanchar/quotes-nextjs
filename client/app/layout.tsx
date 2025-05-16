a89efc03f3f296bc20eff27e9de1e7fcb6394d2e
Taya Hanchar <t.hanchar@andersenlab.com>
2025-05-16 16:48:07 +0300

    feat: add toas component and react-toastify lib

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { NavBar } from "./components/navBar/NavBar";
+import { ToastContainer } from 'react-toastify';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,6 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <ToastContainer aria-label="Error messages" />
         <NavBar />
         <main>
           {children}
--- client/app/search/page.tsx
+++ client/app/search/page.tsx
@@ -3,6 +3,7 @@
 import { useState } from "react"
 import { quoteType } from "../page";
 import { QuoteCard } from "../components/quoteCard/QuoteCard";
+import { toast } from 'react-toastify';
 
 export default function SearchPage() {
   const [title, setTitle] = useState<string>('');
@@ -19,17 +20,26 @@ export default function SearchPage() {
       const response = await fetch(`http://localhost:3000/quotes?${query}&limit=12`);
       const result = await response.json();
       setQuotes(result)
+
     } catch (e) {
       console.log(e)
+      toast.error('Something went wrong!');
     }
+  }
+
+  const handleReset = () => {
+    setQuotes([]);
+    setTitle('');
+    setAuthor('');
   }
 
   return (
     <div>
       <div>
         <input type="text" name="author" placeholder="author" value={author} onChange={(event) => setAuthor(event.target.value)} />
-        <input type="text" name="title" placeholder="title" value={title} onChange={(event) => setTitle(event.target.value)} />
+        <input type="text" name="title" placeholder="text" value={title} onChange={(event) => setTitle(event.target.value)} />
         <button onClick={handleSearch}>Search</button>
+        <button onClick={handleReset}>Reset</button>
       </div>
       <ul className="flex flex-wrap justify-center gap-10 pb-10 pt-10">
         {quotes.map(quote => <QuoteCard key={quote.id} {...quote} />)}
